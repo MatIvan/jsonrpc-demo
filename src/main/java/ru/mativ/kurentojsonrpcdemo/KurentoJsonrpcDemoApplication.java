d5cad5a3967ca1eca643bdcc5e61942ecd1b9991
mativan <matukin.ivan@gmail.com>
2020-05-31 10:12:33 +0300

    First commit. Base Spring Boot Application.

---
+++ src/main/java/ru/mativ/kurentojsonrpcdemo/KurentoJsonrpcDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.mativ.kurentojsonrpcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurentoJsonrpcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurentoJsonrpcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mativ/kurentojsonrpcdemo/KurentoJsonrpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mativ.kurentojsonrpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurentoJsonrpcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
